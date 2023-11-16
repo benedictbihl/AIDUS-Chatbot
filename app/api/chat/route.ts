a5c50d156dfb61ab4a94eb4f82a23401881f5966
Benedict Bihlmaier <Benedict.Bihlmaier@gmail.com>
2023-11-16 09:35:29 +0100

    agent logic updated to use retriever correctly

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -23,37 +23,50 @@ const formatMessage = (message: VercelChatMessage) => {
   return new AIMessage(message.content);
 };
 
+/**
+ * This is how we prime the agent. Here we can also specify a ton of voice, used vocabulary etc.
+ */
 const PREFIX =
   "You are AIDUS, a helpful AI with access to a vast store of knowledge regarding urticaria, a skin condition. You can assume that any questions asked are about urticaria. Please answer them using any tools available to you.";
 
+/**
+ * This is the main function that is called when a user sends a message.
+ * It takes in the message history, and the current message, and returns
+ * a response.
+ * @param req The request object
+ * @returns A streaming response
+ */
 export async function POST(req: Request) {
   const body = await req.json();
   const messages = body.messages ?? [];
   const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
   const currentMessageContent = messages[messages.length - 1].content;
 
-  const { stream, handlers } = LangChainStream();
+  const { stream, handlers } = LangChainStream(); //easy helper to create a stream
 
-  const vectorStore = await VercelPostgres.initialize(
-    new OpenAIEmbeddings(),
-    {}
-  );
-
-  const retriever = vectorStore.asRetriever();
-
-  const tool = createRetrieverTool(retriever, {
-    name: "search_state_of_union",
-    description:
-      "Searches and returns documents regarding the state-of-the-union.",
+  // Initialize the vector store
+  const vectorStore = await VercelPostgres.initialize(new OpenAIEmbeddings(), {
+    tableName: "langchain_pg_embedding",
+    columns: {
+      idColumnName: "uuid",
+      vectorColumnName: "embedding",
+      contentColumnName: "document",
+      metadataColumnName: "cmetadata",
+    },
   });
 
+  // prepare the agent with a retriever, llm and memory
+  const retriever = vectorStore.asRetriever();
+  const tool = createRetrieverTool(retriever, {
+    name: "search_urticaria_scientific_paper",
+    description:
+      "Searches and returns documents regarding urticaria from a body of scientific papers.",
+  });
   const model = new ChatOpenAI({
     temperature: 0,
     streaming: true,
   });
-
   const chatHistory = new ChatMessageHistory(formattedPreviousMessages);
-
   const memory = new OpenAIAgentTokenBufferMemory({
     llm: new ChatOpenAI({}),
     memoryKey: "chat_history",
@@ -61,15 +74,20 @@ export async function POST(req: Request) {
     chatHistory,
   });
 
+  // initialize the agent
   const executor = await initializeAgentExecutorWithOptions([tool], model, {
     agentType: "openai-functions",
     memory,
-    returnIntermediateSteps: true,
+    // returnIntermediateSteps: true,
+    verbose: true, // this will log the agent's internal state, including the tools used
     agentArgs: {
       prefix: PREFIX,
     },
   });
 
+  /* run the agent - it autonomously decides if it needs to call
+   * the retriever or the llm directly, depending on the query
+   */
   executor
     .call(
       {
